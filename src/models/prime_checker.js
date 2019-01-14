8ac27a6bb79c4e3f45cdc70ada4cdcfc7608a521
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-14 17:07:06 +0000

    Add PrimeChecker checkPrimeNumber function.

--- src/models/prime_checker.js
+++ src/models/prime_checker.js
@@ -7,4 +7,17 @@ const PrimeChecker = function() {
 };
 
 
+PrimeChecker.prototype.checkPrimeNumber = function (number) {
+  if (number <= 1) {
+    return 'No!, That is not a prime Number.';
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+        return 'No!, That is not a prime Number.';
+    }
+  }
+  return 'Yes!, That is a prime Number.';
+};
+
+
 module.exports = PrimeChecker;
